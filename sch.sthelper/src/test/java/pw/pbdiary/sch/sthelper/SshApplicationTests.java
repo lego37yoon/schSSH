1d109599fc949559e5cd0595ce5fcf211110bf18
paperbox <lego37yoon@outlook.com>
2021-07-07 18:36:21 +0900

    Init Spring project for Web Application

---
+++ sch.sthelper/src/test/java/pw/pbdiary/sch/sthelper/SshApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.pbdiary.sch.sthelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
