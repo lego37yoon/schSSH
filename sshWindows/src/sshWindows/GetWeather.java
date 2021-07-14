5c4e7550009936041818cd0cdab20a50faf246f6
paperbox <lego37yoon@outlook.com>
2021-07-14 16:58:00 +0900

    Import libraries for connection, exception
    - java.io.IOException
    - java.net.HttpURLConnection
    - java.net.MalformedURLException
    - java.net.URL;

--- sshWindows/src/sshWindows/GetWeather.java
+++ sshWindows/src/sshWindows/GetWeather.java
@@ -1,6 +1,31 @@
 package sshWindows;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.json.*;
 
 public class GetWeather {
+	static String apiKey = "D8y+brPeE6ZR41tB8WEIPi4XUVQo7FZ7dtM/8QrN8y0xQ4r9F/eKsl4GSlZKPwFQYIgDXaPApbQxyU8W/7A6oQ==";
+	static String host = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst";
 	
+	
+	
+	public double getCelsius() {
+		double tem = 0.0;
+		URL url;
+		try {
+			url = new URL (host + "?serviceKey=" + apiKey + "&pageNo=1&numOfRows=10&dataType=JSON&base_date=");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return tem;
+	}
 }
