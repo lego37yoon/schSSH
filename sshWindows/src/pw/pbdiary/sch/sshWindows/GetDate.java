91d6a55ba2dc4081c2229cea0361be9cb6051121
paperbox <lego37yoon@outlook.com>
2021-07-21 16:20:52 +0900

    Convert to Maven Project and Refactoring GetDate.java
    - Convert this project to Maven Project with pom.xml
    - Rewrite GetDate class with java.time.format.DateTimeFormatter

--- sshWindows/src/pw/pbdiary/sch/sshWindows/GetDate.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/GetDate.java
@@ -1,36 +1,18 @@
 package pw.pbdiary.sch.sshWindows;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GetDate {
 	private LocalDateTime today = LocalDateTime.now();
 	
 	public String getTodayDate() {
-		String todayDate = String.valueOf(today.getYear());
-		if (today.getMonthValue() < 10) {
-			todayDate += "0" + today.getMonthValue();
-		} else {
-			todayDate += today.getMonthValue();
-		} //10월 이전은 앞에 0 붙이기
-		
-		if (today.getDayOfMonth() < 10) {
-			todayDate += "0" + today.getDayOfMonth();
-		} else {
-			todayDate += today.getDayOfMonth();
-		} //10일 이전은 앞에 0 붙이기
-		
+		String todayDate = today.format(DateTimeFormatter.BASIC_ISO_DATE);
 		return todayDate;
 	}
 	
 	public String getTodayHour() {
-		String thisHour = "0000";
-		
-		if (today.getHour() < 10) {
-			thisHour = "0" + today.getHour() + "00";
-		} else {
-			thisHour = today.getHour() + "00";
-		}
-		
+		String thisHour = today.format(DateTimeFormatter.ofPattern("kk00"));
 		return thisHour;
 	}
 }
--- sshWindows/src/pw/pbdiary/sch/sshWindows/Main.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/Main.java
@@ -10,6 +10,9 @@ import javax.swing.JTabbedPane;
 import java.awt.Font;
 import javax.swing.JList;
 import javax.swing.JInternalFrame;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Main {
 
@@ -47,11 +50,21 @@ public class Main {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 800, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{355, 69, 1, 0};
+		gridBagLayout.rowHeights = new int[]{36, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JTabbedPane noticeTab = new JTabbedPane(JTabbedPane.TOP);
 		noticeTab.setFont(new Font("Noto Sans CJK KR Light", Font.PLAIN, 14));
-		frame.getContentPane().add(noticeTab);
+		GridBagConstraints gbc_noticeTab = new GridBagConstraints();
+		gbc_noticeTab.anchor = GridBagConstraints.NORTHWEST;
+		gbc_noticeTab.insets = new Insets(0, 0, 0, 5);
+		gbc_noticeTab.gridx = 1;
+		gbc_noticeTab.gridy = 0;
+		frame.getContentPane().add(noticeTab, gbc_noticeTab);
 		
 		JList normalNotice = new JList();
 		normalNotice.setFont(new Font("Noto Sans CJK KR Light", Font.PLAIN, 14));
@@ -59,7 +72,11 @@ public class Main {
 		
 		JList todoList = new JList();
 		todoList.setFont(new Font("Noto Sans CJK KR Light", Font.PLAIN, 14));
-		frame.getContentPane().add(todoList);
+		GridBagConstraints gbc_todoList = new GridBagConstraints();
+		gbc_todoList.anchor = GridBagConstraints.WEST;
+		gbc_todoList.gridx = 2;
+		gbc_todoList.gridy = 0;
+		frame.getContentPane().add(todoList, gbc_todoList);
 	}
 
 }
