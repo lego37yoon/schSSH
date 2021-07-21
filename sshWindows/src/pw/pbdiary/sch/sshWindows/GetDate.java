cbbee1495621aad1e73344531e2ba57fab17056f
paperbox <lego37yoon@outlook.com>
2021-07-21 17:25:12 +0900

    Return previous hour for Korea Forecast System
    - Korean Weather Forecast System provides current weather information in current houir at every 40 minutes.
    - because of its system works, return previous hour when less than 40 minutes of hour now.

--- sshWindows/src/pw/pbdiary/sch/sshWindows/GetDate.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/GetDate.java
@@ -12,7 +12,14 @@ public class GetDate {
 	}
 	
 	public String getTodayHour() {
-		String thisHour = today.format(DateTimeFormatter.ofPattern("kk00"));
+		//기상청 초단기실황 API 특성 상 현재 시각 기준 40분부터 API 제공하므로
+		//그 전까지는 1시간 이전 정보 제공
+		String thisHour = "0000";
+		if (today.getMinute() < 41) {
+			thisHour = today.minusHours(1).format(DateTimeFormatter.ofPattern("kk00"));
+		} else {
+			thisHour = today.format(DateTimeFormatter.ofPattern("kk00"));
+		}
 		return thisHour;
 	}
 }
--- sshWindows/src/pw/pbdiary/sch/sshWindows/GetWeather.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/GetWeather.java
@@ -1,17 +1,15 @@
 package pw.pbdiary.sch.sshWindows;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.nio.charset.StandardCharsets;
 
 import org.json.*;
 
 public class GetWeather {
-	static final String apiKey = "D8y+brPeE6ZR41tB8WEIPi4XUVQo7FZ7dtM/8QrN8y0xQ4r9F/eKsl4GSlZKPwFQYIgDXaPApbQxyU8W/7A6oQ==";
+	static final String apiKey = "D8y%2BbrPeE6ZR41tB8WEIPi4XUVQo7FZ7dtM%2F8QrN8y0xQ4r9F%2FeKsl4GSlZKPwFQYIgDXaPApbQxyU8W%2F7A6oQ%3D%3D";
 	static final String host = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst";
 	static final int sinchang_x = 59;
 	static final int sinchang_y = 110;
@@ -33,13 +31,7 @@ public class GetWeather {
 			connection.setRequestMethod("GET"); //GET 모드로 설정
 			connection.setRequestProperty("Content-type", "application/json");
 			
-			BufferedReader weatherData; //raw 데이터 담을 Buffered Reader 생성
-			
-			if (connection.getResponseCode() >= 200 && connection.getResponseCode() <= 300) {
-				weatherData = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			} else {
-				weatherData = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-			}
+			String weatherData = new String(connection.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
 			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
