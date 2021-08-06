11649c542dbcca4ffcdc2654c70bc2624d065225
paperbox <lego37yoon@outlook.com>
2021-08-06 17:02:17 +0900

    tiny fixes

--- sshWindows/src/pw/pbdiary/sch/sshWindows/LoginToSchool.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/LoginToSchool.java
@@ -1,5 +1,24 @@
 package pw.pbdiary.sch.sshWindows;
 
-public class LoginToSchool {
+import java.io.IOException;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+public class LoginToSchool {
+	private static final String ua = "";
+	
+	private void loginViaAuth(String id, String pw) {
+		try {
+			Connection.Response loginData = Jsoup.connect("https://sso.sch.ac.kr/oa/au/auth/verify")
+					.userAgent(ua)
+					.referrer("https://sso.sch.ac.kr/oa/au/auth?login_endpoint=oauth&retUrl=")
+					.followRedirects(true)
+					.method(Connection.Method.POST)
+					.execute();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
