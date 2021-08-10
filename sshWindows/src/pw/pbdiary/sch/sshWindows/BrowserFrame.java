5fe5438cb5a6de85790e20137854e2bde06d039a
paperbox <lego37yoon@outlook.com>
2021-08-10 12:33:00 +0900

    Add BrowserFrame.java (working)
    - Add BrowserFrame.java with JCEF by JetBrains

--- sshWindows/src/pw/pbdiary/sch/sshWindows/BrowserFrame.java
+++ sshWindows/src/pw/pbdiary/sch/sshWindows/BrowserFrame.java
@@ -1,5 +1,20 @@
 package pw.pbdiary.sch.sshWindows;
 
-public class BrowserFrame {
+import javax.swing.JFrame;
 
+import org.cef.browser.CefBrowser;
+
+public class BrowserFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private CefBrowser browser_ = null;
+	
+	public BrowserFrame() {
+		this(null);
+	}
+
+	public BrowserFrame(String title) {
+		super(title);
+		
+	}
 }
